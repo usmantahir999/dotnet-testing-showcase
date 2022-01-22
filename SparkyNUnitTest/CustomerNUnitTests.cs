f11c22f395b62ec54c5fdb71aec2069502f67e74
Usman Tahir <Usman.tahir@synergy-it.com>
2022-01-22 20:45:09 +0500

    Add CombineName_InputFirstAndLastName_ReturnsFullName

---
+++ SparkyNUnitTest/CustomerNUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyNUnitTest
+{
+    [TestFixture]
+    public class CustomerNUnitTests
+    {
+        [Test]
+        public void CombineName_InputFirstAndLastName_ReturnsFullName()
+        {
+            //Arrange
+            var customer = new Customer();
+            //Act
+            var result = customer.GreetAndCombineNames("Ben","Spark");
+            Assert.That(result, Is.EqualTo("Hello Ben Spark"));
+        }
+    }
+}
