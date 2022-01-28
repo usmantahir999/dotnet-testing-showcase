c783d2094200c2769160388147fbf7e597901315
Usman Tahir <Usman.tahir@synergy-it.com>
2022-01-28 21:28:16 +0500

    BankWithDraw_WithDrawl100With200Balance_ReturnsTrue

--- SparkyNUnitTest/BankAccountNUnitTests.cs
+++ SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -21,15 +21,15 @@ namespace SparkyNUnitTest
             
         }
 
-        [Test]
-        public void BankDepositLogFakker_Add100_ReturnTrue()
-        {
-            //in real project faker class can not be implemented, because it takes alot of time and too much dummy code
-            BankAccount bankAccount = new BankAccount(new LogFakker());
-            var result=bankAccount.Deposit(100);
-            Assert.IsTrue(result);
-            Assert.That(bankAccount.GetBalance(), Is.EqualTo(100));
-        }
+        //[Test]
+        //public void BankDepositLogFakker_Add100_ReturnTrue()
+        //{
+        //    //in real project faker class can not be implemented, because it takes alot of time and too much dummy code
+        //    BankAccount bankAccount = new BankAccount(new LogFakker());
+        //    var result=bankAccount.Deposit(100);
+        //    Assert.IsTrue(result);
+        //    Assert.That(bankAccount.GetBalance(), Is.EqualTo(100));
+        //}
 
         [Test]
         public void BankDeposit_Add100_ReturnTrue()
@@ -39,6 +39,21 @@ namespace SparkyNUnitTest
             var result = bankAccount.Deposit(100);
             Assert.IsTrue(result);
             Assert.That(bankAccount.GetBalance(), Is.EqualTo(100));
+        }
+
+        [Test]
+        [TestCase(200,100)]
+        public void BankWithDraw_WithDrawl100With200Balance_ReturnsTrue(int balance, int withDrawlAmount)
+        {
+            var logMoq = new Mock<ILogBook>();
+            logMoq.Setup(u => u.LogToDb(It.IsAny<string>())).Returns(true);
+            logMoq.Setup(u => u.LogBalanceAfterWithDrawl(It.Is<int>(x=>x>0))).Returns(true);
+            BankAccount bankAccount = new BankAccount(logMoq.Object);
+            bankAccount.Deposit(balance);
+            var result = bankAccount.WithDraw(withDrawlAmount);
+            Assert.IsTrue(result);
+
+
         }
     }
 }
