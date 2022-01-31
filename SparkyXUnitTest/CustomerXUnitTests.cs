0b7450a6a616b825771c049e431a6222b6eed749
Usman Tahir <Usman.tahir@synergy-it.com>
2022-01-31 23:04:37 +0500

    CustomerxUnitTests

--- SparkyXUnitTest/CustomerXUnitTests.cs
+++ SparkyXUnitTest/CustomerXUnitTests.cs
@@ -1,77 +1,75 @@
-﻿//using NUnit.Framework;
-//using Sparky;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
-//namespace SparkyXUnitTest
-//{
-//    [TestFixture]
-//    public class CustomerxUnitTests
-//    {
-//        private Customer customer;
-//        [SetUp]
-//        public void Setup()
-//        {
-//            //Arrange
-//            customer = new Customer();
-//        }
+namespace SparkyXUnitTest
+{
+    public class CustomerxUnitTests
+    {
+        private Customer customer;
 
-//        [Test]
-//        public void CombineName_InputFirstAndLastName_ReturnsFullName()
-//        {
-//            //Act
-//            var result = customer.GreetAndCombineNames("Ben","Spark");
-//            Assert.That(result, Is.EqualTo("Hello Ben Spark"));
-//        }
+        public CustomerxUnitTests()
+        {
+            //Arrange
+            customer = new Customer();
+        }
 
-//        [Test]
-//        public void IsHelloExist_InputFirstAndLastName_ReturnsTrue()
-//        {
-//            //Act
-//            var result = customer.GreetAndCombineNames("Ben", "Spark");
-//            Assert.That(result, Does.Contain("Hello"));
-//        }
+        [Fact]
+        public void CombineName_InputFirstAndLastName_ReturnsFullName()
+        {
+            //Act
+            var result = customer.GreetAndCombineNames("Ben", "Spark");
+            Assert.Equal( "Hello Ben Spark", result);
+        }
 
-//        [Test]
-//        public void IsGreetNull_InputNull_ReturnsNull()
-//        {
-//            //Act
-//            //Assert
-//            Assert.IsNull(customer.GreetMessage);
-//        }
+        [Fact]
+        public void IsHelloExist_InputFirstAndLastName_ReturnsTrue()
+        {
+            //Act
+            var result = customer.GreetAndCombineNames("Ben", "Spark");
+            Assert.Contains( "Hello",result);
+        }
 
-//        [Test]
-//        public void IsGreetNotNull_InputFirstAndLastName_ReturnsNotNull()
-//        {
-//            //Act
-//            customer.GreetAndCombineNames("Ben", "Spark");
-//            //Assert
-//            Assert.IsNotNull(customer.GreetMessage);
-//        }
+        [Fact]
+        public void IsGreetNull_InputNull_ReturnsNull()
+        {
+            //Act
+            //Assert
+            Assert.Null(customer.GreetMessage);
+        }
 
-//        [Test]
-//        public void DiscountChecker_DefaultCustomer_ReturnsDiscountInRange()
-//        {
-//            //Act
-//            var result=customer.Discount;
-//            //Assert
-//            //Assert.multiple used to run multiple assert statements in exception cases
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(result, Is.InRange(10, 25));
-//            });
+        [Fact]
+        public void IsGreetNotNull_InputFirstAndLastName_ReturnsNotNull()
+        {
+            //Act
+            customer.GreetAndCombineNames("Ben", "Spark");
+            //Assert
+            Assert.NotNull(customer.GreetMessage);
+        }
+
+        [Fact]
+        public void DiscountChecker_DefaultCustomer_ReturnsDiscountInRange()
+        {
+            //Act
+            var result = customer.Discount;
+            //Assert
+            //Assert.multiple NOT used to run multiple assert statements in exception cases in XUNIT
            
-//        }
+                Assert.InRange(result, 10, 25);
 
-//        [Test]
-//        public void GreetChecker_EmptyFirstName_ThrowsException()
-//        {
-//            var exceptionDetails=Assert.Throws<ArgumentException>(()=>customer.GreetAndCombineNames("", "Spark"));
-//            Assert.AreEqual("Empty first name", exceptionDetails.Message);
 
-//        }
-//    }
-//}
+        }
+
+        [Fact]
+        public void GreetChecker_EmptyFirstName_ThrowsException()
+        {
+            var exceptionDetails = Assert.Throws<ArgumentException>(() => customer.GreetAndCombineNames("", "Spark"));
+            Assert.Equal("Empty first name", exceptionDetails.Message);
+
+        }
+    }
+}
