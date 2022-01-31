e4aa233671151510914c2b16bcb2d7a24ae32e28
Usman Tahir <Usman.tahir@synergy-it.com>
2022-01-31 22:59:01 +0500

    ProductXUnitTests

--- SparkyXUnitTest/ProductXUnitTests.cs
+++ SparkyXUnitTest/ProductXUnitTests.cs
@@ -1,22 +1,22 @@
-﻿//using NUnit.Framework;
-//using Sparky;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
-//namespace SparkyXUnitTest
-//{
-//    [TestFixture]
-//    class ProductXUnitTests
-//    {
-//        [Test]
-//        public void GetPricePrice_PlatinumCustomer_ReturnsPrice20PercentDiscount()
-//        {
-//            Product product = new Product() { Price = 50 };
-//            var result=product.GetPrice(new Customer() { IsPlatium = true });
-//            Assert.That(result, Is.EqualTo(40));
-//        }
-//    }
-//}
+namespace SparkyXUnitTest
+{
+    
+    public class ProductXUnitTests
+    {
+        [Fact]
+        public void GetPricePrice_PlatinumCustomer_ReturnsPrice20PercentDiscount()
+        {
+            Product product = new Product() { Price = 50 };
+            var result = product.GetPrice(new Customer() { IsPlatium = true });
+            Assert.Equal(40,result);
+        }
+    }
+}
