a4fbd815eb45830c225f00ee41b1958e04f4ff4e
Usman Tahir <Usman.tahir@synergy-it.com>
2022-02-02 17:17:20 +0500

    DateValidator_InputeExpextedDateRange_DateValidity

---
+++ Bongo_InitialSetup (.NET 5)/Bongo.Models.Tests/DateInFutureAttributeTests.cs
@@ -0,0 +1,24 @@
+﻿using Bongo.Models.ModelValidations;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bongo.Models.Tests
+{
+    [TestFixture]
+    public class DateInFutureAttributeTests
+    {
+    
+        [Test]
+        public void DateValidator_InputeExpextedDateRange_DateValidity()
+        {
+            DateInFutureAttribute dateInFutureAttribute = new DateInFutureAttribute(()=>DateTime.Now);
+            var result = dateInFutureAttribute.IsValid(DateTime.Now.AddSeconds(100));
+            Assert.AreEqual(true, result);
+        }
+    }
+   
+}
