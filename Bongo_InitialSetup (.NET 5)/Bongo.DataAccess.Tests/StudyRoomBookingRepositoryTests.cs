8a2237144e832be5440a192d04d7567f9094a6ab
Usman Tahir <Usman.tahir@synergy-it.com>
2022-02-02 22:14:18 +0500

    GetAllBooking_BookingOneAndTwo_CheckBothFromDatabase

--- Bongo_InitialSetup (.NET 5)/Bongo.DataAccess.Tests/StudyRoomBookingRepositoryTests.cs
+++ Bongo_InitialSetup (.NET 5)/Bongo.DataAccess.Tests/StudyRoomBookingRepositoryTests.cs
@@ -3,6 +3,7 @@ using Bongo.Models.Model;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -69,6 +70,50 @@ namespace Bongo.DataAccess.Tests
                 Assert.AreEqual(_studyRoomBookingOne.Date, bookingFromDb.Date);
                 Assert.AreEqual(_studyRoomBookingOne.StudyRoomId, bookingFromDb.StudyRoomId);
             }
-        }    
+        }
+
+        [Test]
+
+        public void GetAllBooking_BookingOneAndTwo_CheckBothFromDatabase()
+        {
+            //arrange
+            var expectedResult = new List<StudyRoomBooking> { _studyRoomBookingOne, _studyRoomBookingTwo };
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "temp_Bongo").Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var repository = new StudyRoomBookingRepository(context);
+                repository.Book(_studyRoomBookingOne);
+                repository.Book(_studyRoomBookingTwo);
+            }
+
+            //act
+            List<StudyRoomBooking> actualResult;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var repository = new StudyRoomBookingRepository(context);
+                actualResult=repository.GetAll(null).ToList();
+            }
+
+            //assert
+
+            CollectionAssert.AreEqual(expectedResult, actualResult, new BookingComparer());
+        }
+    }
+
+    public class BookingComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            var bookingObj1 = (StudyRoomBooking)x;
+            var bookingObj2 = (StudyRoomBooking)y;
+
+            if (bookingObj1.BookingId != bookingObj2.BookingId)
+            {
+                return 1;
+            }
+            return 0;
+        }
     }
 }
