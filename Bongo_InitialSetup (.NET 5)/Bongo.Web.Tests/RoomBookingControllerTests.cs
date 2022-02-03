dfe569594f328894a346f2921311cded8e55ce9f
Usman Tahir <Usman.tahir@synergy-it.com>
2022-02-03 21:19:53 +0500

    IndexPage_CallRequest_VerifyGetAllInvoked

---
+++ Bongo_InitialSetup (.NET 5)/Bongo.Web.Tests/RoomBookingControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Bongo.Core.Services.IServices;
+using Bongo.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bongo.Web.Tests
+{
+    [TestFixture]
+    public class RoomBookingControllerTests
+    {
+        private Mock<IStudyRoomBookingService> _studyRoomBookingService;
+        private RoomBookingController _roomBookingController;
+
+        [SetUp]
+
+        public void Setup()
+        {
+            _studyRoomBookingService = new Mock<IStudyRoomBookingService>();
+            _roomBookingController = new RoomBookingController(_studyRoomBookingService.Object);
+        }
+
+        [Test]
+        public void IndexPage_CallRequest_VerifyGetAllInvoked()
+        {
+            _roomBookingController.Index();
+            _studyRoomBookingService.Verify(u => u.GetAllBooking(), Times.Once);
+        }
+    }
+}
